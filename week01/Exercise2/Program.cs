a9e51621aeee6ba809d8e8205f21f700ddfa01d8
ronon10 <renan.freitas100@gmail.com>
2025-01-11 09:02:59 -0300

    week1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What was your percentage grade?: ");
+        string valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "Congrats - A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "Congrats - B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "Congrats - C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "You dont pass - D";
+        }
+        else 
+        {
+            letter = "Tray again - F";
+        }
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congradulations!! You pass");
+        }
+        else
+        {
+            Console.WriteLine("Tray again next semester");
+        }
+    
+    
+    
+    
     }
+
 }
